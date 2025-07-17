a376b54b2094064777283bd4d6c896ce6ee2d252
Maybewuss <38156589+Maybewuss@users.noreply.github.com>
2025-07-17 18:00:31 +0800

    fix batched_async_rm

--- slime/rollout/rm_hub/__init__.py
+++ slime/rollout/rm_hub/__init__.py
@@ -59,19 +59,6 @@ async def batched_async_rm(
     samples: list[Sample],
     **kwargs,
 ) -> list[Union[int, float]]:
-    if args.custom_rm_path is not None:
-        rm_function = load_function(args.custom_rm_path)
-        return await rm_function(args, samples, **kwargs)
-
-    rm_type = args.rm_type
-    prompts = [sample.prompt for sample in samples]
-    responses = [sample.response for sample in samples]
-    labels = [sample.label for sample in samples]
-    if labels is None:
-        labels = [None] * len(prompts)
-    tasks = [
-        async_rm(rm_type, prompt, response, label, **kwargs)
-        for prompt, response, label in zip(prompts, responses, labels)
-    ]
+    tasks = [async_rm(args, sample, **kwargs) for sample in samples]
     rewards = await asyncio.gather(*tasks)
     return rewards
