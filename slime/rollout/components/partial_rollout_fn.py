50445c670f22bebf9c4b9a483a14865140a84295
fzyzcjy <ch271828n@outlook.com>
2025-08-13 09:52:25 +0800

    more

---
+++ slime/rollout/components/partial_rollout_fn.py
@@ -0,0 +1,9 @@
+from slime.rollout.components.base_rollout_fn import RolloutFnInitParams, RolloutFnCallParams, RolloutFnCallOutput
+
+
+class PartialRolloutFn:
+    def __init__(self, params: RolloutFnInitParams):
+        TODO
+
+    def __call__(self, params: RolloutFnCallParams) -> RolloutFnCallOutput:
+        TODO
