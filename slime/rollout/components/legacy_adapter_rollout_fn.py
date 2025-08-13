7cc3207f6539b975785f6fd4a7b8a6d4a55afdd2
fzyzcjy <ch271828n@outlook.com>
2025-08-13 08:36:10 +0800

    more

---
+++ slime/rollout/components/legacy_adapter_rollout_fn.py
@@ -0,0 +1,9 @@
+from slime.rollout.components.base_rollout_fn import RolloutFnInitParams, RolloutFnCallParams, RolloutFnCallOutput
+
+
+class LegacyAdapterRolloutFn:
+    def __init__(self, params: RolloutFnInitParams):
+        TODO
+
+    def __call__(self, params: RolloutFnCallParams) -> RolloutFnCallOutput:
+        return TODO
