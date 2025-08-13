5fdfdab276303efaee64f45d86be8b92b4fbac20
fzyzcjy <ch271828n@outlook.com>
2025-08-13 08:01:17 +0800

    more

--- train_async.py
+++ train_async.py
@@ -1,4 +1,5 @@
 import slime.entrypoints.train_async
 
 if __name__ == '__main__':
+    print("WARN: This entrypoint is deprecated. Please use slime.entrypoints.* instead.")
     slime.entrypoints.train_async.run()
