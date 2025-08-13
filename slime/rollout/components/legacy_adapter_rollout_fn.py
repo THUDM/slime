626e9da5bb2b7939bb865d7c2c454a8d793ffb36
fzyzcjy <ch271828n@outlook.com>
2025-08-13 08:49:51 +0800

    more

--- slime/rollout/components/legacy_adapter_rollout_fn.py
+++ slime/rollout/components/legacy_adapter_rollout_fn.py
@@ -8,12 +8,13 @@ class LegacyAdapterRolloutFn:
         print("Using legacy format for rollout fn.")
         self.original_fn = original_fn
         self.init_params = params
+        self._legacy_data_buffer_adapter = _LegacyDataBufferAdapter()
 
     def __call__(self, params: RolloutFnCallParams) -> RolloutFnCallOutput:
         raw_output = self.original_fn(
             self.init_params.args,
             params.rollout_id,
-            TODO,
+            self._legacy_data_buffer_adapter,
             evaluation=self.init_params.evaluation,
         )
 
@@ -21,3 +22,6 @@ class LegacyAdapterRolloutFn:
             return RolloutFnCallOutput(samples=None, metrics=raw_output)
         else:
             return RolloutFnCallOutput(samples=raw_output, metrics=None)
+
+class _LegacyDataBufferAdapter:
+    TODO
