a02f65e03caf4fe7c12fb8b7611380fc6348f1f6
fzyzcjy <ch271828n@outlook.com>
2025-08-13 08:03:09 +0800

    fmt

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import slime.entrypoints.train
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("WARN: This entrypoint is deprecated. Please use slime.entrypoints.* instead.")
     slime.entrypoints.train.run()
