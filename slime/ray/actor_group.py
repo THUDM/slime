a6a4f1242ea6c0b7964bbb40abbee6955c266b89
Ata Fatahi <immrata@gmail.com>
2025-09-02 17:26:58 -0700

    fix resource value type
    Signed-off-by: Ata Fatahi <immrata@gmail.com>

--- slime/ray/actor_group.py
+++ slime/ray/actor_group.py
@@ -24,7 +24,7 @@ class RayTrainGroup:
         wandb_run_id (str, optional): Weights and biases run id. Defaults to None.
         num_gpus_per_actor (float, optional): Number of gpus allocated for each actor.
             If < 1.0, multiple models can share same gpu. Defaults to 1.
-        resources (Dict[str, int], optional): Custom resources to allocate for each actor.
+        resources (Dict[str, float], optional): Custom resources to allocate for each actor.
             See https://docs.ray.io/en/latest/ray-core/scheduling/resources.html
         num_resources_per_node (int, optional): Number of custom resources to allocate for each node.
             See https://docs.ray.io/en/latest/ray-core/scheduling/resources.html
