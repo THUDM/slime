663fd0bda2f6469041b5a3f6a38854e9066c419c
zlHuang <108509803+zlH518@users.noreply.github.com>
2025-10-29 18:05:28 +0800

    Implement benchmark_func for performance measurement
    Added a benchmarking function to measure execution time and performance metrics.

--- slime/utils/profile_utils.py
+++ slime/utils/profile_utils.py
@@ -1,7 +1,8 @@
 import time
-from pathlib import Path
-
 import torch
+
+from pathlib import Path
+from torch.profiler import profile, ProfilerActivity
 
 
 # The memory_snapshot_path is not a full path, but we name like this to be compatible with megatron
@@ -33,3 +34,73 @@ def get_memory_snapshot_full_path(args):
         Path(args.memory_snapshot_dir)
         / f"memory_snapshot_time{time.time()}_rank{torch.distributed.get_rank()}_{args.memory_snapshot_path}"
     )
+
+def benchmark_func(
+    fn,
+    *args,
+    n_warmup=10,
+    n_repeat=100,
+    ref_flops=None,
+    ref_bytes=None,
+    ref_time=None,
+    n_profile=0,
+    trace_dir=None,
+    name="",
+    **kwargs,
+):
+    func_name = getattr(fn, "__name__", None)
+    func_name = name if func_name == "apply" or func_name is None else func_name
+
+    for i in range(n_warmup):
+        fn(*args, **kwargs)
+
+    start_events = [torch.cuda.Event(enable_timing=True) for _ in range(n_repeat)]
+    end_events = [torch.cuda.Event(enable_timing=True) for _ in range(n_repeat)]
+
+    ts = time.time()
+    for i in range(n_repeat):
+        start_events[i].record()
+        fn(*args, **kwargs)
+        end_events[i].record()
+
+    torch.cuda.synchronize()
+
+    if n_profile > 0:
+        with profile(
+            activities=[
+                ProfilerActivity.CPU,
+                ProfilerActivity.CUDA,
+                ProfilerActivity.XPU,
+            ]
+        ) as prof:
+            for i in range(n_profile):
+                fn(*args, **kwargs)
+        print(
+            prof.key_averages().table(
+                sort_by="cuda_time_total", top_level_events_only=True, row_limit=100
+            )
+        )
+        if trace_dir is not None:
+            assert trace_dir.endswith(".json")
+            prof.export_chrome_trace(trace_dir)
+
+    times = [s.elapsed_time(e) for s, e in zip(start_events, end_events)]
+    times = sorted(times)
+    clip = max(1, n_repeat // 100)
+    times = sum(times[clip:-clip])
+
+    average_event_time = times * 1000 / (n_repeat - 2 * clip)
+
+    fs = ""
+    if ref_flops is not None:
+        flops = ref_flops / 1e12 / (average_event_time / 1e6)
+        fs = f"FLOPS:{flops:.2f}T"
+    bs = ""
+    if ref_bytes is not None:
+        bs = f"bandwidth:{ref_bytes / average_event_time / 1e3:.1f}G/S"
+    ss = ""
+    if ref_time is not None:
+        ss = f"speedup:{ref_time / average_event_time:.3f}"
+
+    print(f"{func_name:<30} {name} time:{average_event_time:.1f} us {fs} {bs} {ss}")
+    return average_event_time
