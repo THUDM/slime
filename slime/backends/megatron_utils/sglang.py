e1413e16c4ba2f7c82b797a68e7d0737130dd05b
Zilin Zhu <zhuzilinallen@gmail.com>
2025-12-04 10:06:56 +0000

    remove use_flattened_tensor_bucket for cleaner code

--- slime/backends/megatron_utils/sglang.py
+++ slime/backends/megatron_utils/sglang.py
@@ -17,16 +17,9 @@ from sglang.srt.utils import MultiprocessingSerializer
 
 
 try:
-    try:
-        from sglang.srt.weight_sync.tensor_bucket import FlattenedTensorBucket  # type: ignore[import]
-    except ImportError:
-        from sglang.srt.model_executor.model_runner import FlattenedTensorBucket  # type: ignore[import]
-
-    use_flattened_tensor_bucket = True
-except Exception:
-    FlattenedTensorBucket = None
-    use_flattened_tensor_bucket = False
-
+    from sglang.srt.weight_sync.tensor_bucket import FlattenedTensorBucket  # type: ignore[import]
+except ImportError:
+    from sglang.srt.model_executor.model_runner import FlattenedTensorBucket  # type: ignore[import]
 
 __all__ = [
     "quant_weight_ue8m0",
@@ -35,5 +28,4 @@ __all__ = [
     "monkey_patch_torch_reductions",
     "MultiprocessingSerializer",
     "FlattenedTensorBucket",
-    "use_flattened_tensor_bucket",
 ]
