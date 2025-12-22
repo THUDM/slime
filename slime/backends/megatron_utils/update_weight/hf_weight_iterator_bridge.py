5da09d091049ba78593c7d48a5701cf3f58c2dbe
nanjiangwill <willjiang2018@gmail.com>
2025-12-22 04:10:49 -0500

    fix script

--- slime/backends/megatron_utils/update_weight/hf_weight_iterator_bridge.py
+++ slime/backends/megatron_utils/update_weight/hf_weight_iterator_bridge.py
@@ -1,7 +1,4 @@
 import dataclasses
-import re
-
-import torch
 
 from slime.utils import megatron_bridge_utils
 from slime.utils.iter_utils import chunk_named_params_by_size
@@ -15,9 +12,10 @@ class HfWeightIteratorBridge(HfWeightIteratorBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        from megatron.bridge import AutoBridge
         import slime_plugins.megatron_bridge  # noqa: F401
 
-        self._bridge = args.bridge
+        self._bridge = AutoBridge.from_hf_pretrained(self.args.hf_checkpoint)
 
     def get_hf_weight_chunks(self, megatron_local_weights):
         # TODO support quantization (e.g. modify megatron-bridge to provide megatron param name)
@@ -44,61 +42,7 @@ class HfWeightIteratorBridge(HfWeightIteratorBase):
             yield from chunk_named_params_by_size(named_weights, chunk_size=self.args.update_weight_buffer_size)
 
 
-def _has_expert_weights(tasks):
-    """Check if any task has param_name ending with weight<NUMBER>."""
-    expert_pattern = re.compile(r"^.+\.weight\d+$")
-    return any(task.param_weight is not None and expert_pattern.match(task.param_name) for task in tasks)
-
-
-def _merge_expert_tasks(tasks, new_weight_dict):
-    """Merge expert tasks (weight0, weight1, ...) into single tasks with stacked tensors."""
-    expert_pattern = re.compile(r"^(.+\.weight)(\d+)$")
-    expert_groups = {}  # (vp_stage, base_param_name) -> [(expert_idx, task)]
-    non_expert_tasks = []
-
-    for task in tasks:
-        if task.param_weight is None:
-            non_expert_tasks.append(task)
-            continue
-
-        match = expert_pattern.match(task.param_name)
-        if match:
-            base_param_name = match.group(1)
-            expert_idx = int(match.group(2))
-            group_key = (task.vp_stage, base_param_name)
-            expert_groups.setdefault(group_key, []).append((expert_idx, task))
-        else:
-            non_expert_tasks.append(task)
-
-    # Create merged tasks
-    merged_tasks = []
-    for (_, base_param_name), expert_list in expert_groups.items():
-        expert_list.sort(key=lambda x: x[0])  # Sort by expert_idx numerically
-
-        cpu_tensors = [
-            new_weight_dict[f"vp_stages.{task.vp_stage}.{task.param_name}"].transpose(0, 1) for _, task in expert_list
-        ]
-        merged_tensor = torch.stack(cpu_tensors, dim=1).cuda()
-        # Use first expert's task as template for merged task
-        template_task = expert_list[0][1]  # expert_list = [(expert_idx, task), ...]
-        merged_task = dataclasses.replace(template_task, param_name=base_param_name, param_weight=merged_tensor)
-        merged_tasks.append(merged_task)
-
-    return non_expert_tasks, merged_tasks
-
-
 def _process_conversion_tasks(vanilla_conversion_tasks, new_weight_dict):
-    """Process conversion tasks, merging expert weights if present to avoid OOM."""
-    all_tasks = list(vanilla_conversion_tasks)
-
-    # Check if expert merging is needed
-    if _has_expert_weights(all_tasks):
-        non_expert_tasks, merged_tasks = _merge_expert_tasks(all_tasks, new_weight_dict)
-    else:
-        non_expert_tasks = all_tasks
-        merged_tasks = []
-
-    # Move weights to CUDA for non-expert tasks
     def _handle_one(task):
         if task.param_weight is None:
             return task
@@ -112,8 +56,7 @@ def _process_conversion_tasks(vanilla_conversion_tasks, new_weight_dict):
         new_param_weight = new_param_weight.cuda()
         return dataclasses.replace(task, param_weight=new_param_weight)
 
-    processed_non_expert = [_handle_one(task) for task in non_expert_tasks]
-    return _MapWithLen(lambda x: x, processed_non_expert + merged_tasks)
+    return _MapWithLen(_handle_one, vanilla_conversion_tasks)
 
 
 class _MapWithLen:
