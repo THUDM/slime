43da5e2cbdc76680c3e5c3c14ea8cc97ca73c8e8
miles.pr.bot <miles.pr.bot@gmail.com>
2025-12-31 18:43:14 -0800

    [sync] Super tiny update link (#312)

--- tests/test_qwen3_0.6B_fsdp_distributed.py
+++ tests/test_qwen3_0.6B_fsdp_distributed.py
@@ -1,4 +1,3 @@
-import os
 import slime.utils.external_utils.command_utils as U
 
 MODEL_NAME = "Qwen3-0.6B"
@@ -100,8 +99,4 @@ def execute():
 
 if __name__ == "__main__":
     prepare()
-    os.environ.pop("http_proxy")
-    os.environ.pop("https_proxy")
-    os.environ.pop("HTTP_PROXY")
-    os.environ.pop("HTTPS_PROXY")
     execute()
