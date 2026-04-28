5702bad09446a2891ff24d4a392d4b7ce8bb8d9d
Zilin Zhu <zhuzilinallen@gmail.com>
2026-04-28 11:20:22 +0000

    bugfix

--- slime/utils/megatron_bridge_utils.py
+++ slime/utils/megatron_bridge_utils.py
@@ -6,6 +6,28 @@ except ImportError:
     unwrap_model = None
 
 
+def patch_hf_config_for_megatron_bridge(hf_config):
+    configs = [hf_config]
+    text_config = getattr(hf_config, "text_config", None)
+    if text_config is not None and text_config is not hf_config:
+        configs.append(text_config)
+
+    for config in configs:
+        rope_params = getattr(config, "rope_parameters", None)
+        if isinstance(rope_params, dict) and "rope_theta" in rope_params and not hasattr(config, "rope_theta"):
+            config.rope_theta = rope_params["rope_theta"]
+
+    return hf_config
+
+
+def patch_auto_bridge_hf_config(bridge):
+    hf_pretrained = getattr(bridge, "hf_pretrained", None)
+    if hf_pretrained is not None:
+        patch_hf_config_for_megatron_bridge(hf_pretrained)
+
+    return bridge
+
+
 @contextmanager
 def patch_megatron_model(model):
     unwrapped_model = unwrap_model(model)[0]
