a3f500977f33b82de6ac0414c0c43abfe656c3e0
Zilin Zhu <zhuzilinallen@gmail.com>
2026-08-26 14:23:34 +0700

    [docs] fix out-dated doc (#2330)

--- slime/utils/arguments.py
+++ slime/utils/arguments.py
@@ -657,8 +657,9 @@ def get_slime_extra_args_provider(add_custom_arguments=None):
                 default=None,
                 help=(
                     "The path to the prompt data. "
-                    "Currently we only support jsonl format, and each line should contains --input-key and --label-key, "
-                    "which will be used as the prompt and the label respectively. "
+                    "Supported formats are JSONL and Parquet (Parquet requires pyarrow). "
+                    "Each record should contain --input-key and --label-key, which will be used as the prompt and "
+                    "the label respectively. "
                     "If you want to use a custom template, you can set --apply-chat-template to true, in that case, "
                     "the input should be the same structure as an openai message, e.g. [{'role': 'user', 'content': 'blabla'}]. "
                 ),
